563860c7d6809ac85590f7495a3c28606233087e
Trey Shugart <treshugart@gmail.com>
2016-08-11 00:27:22 +1000

    feat: Add necessary polyfills for Skate web components. Ready for initial release.

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import 'skatejs-named-slots';
+import 'es6-collections';
+import 'webcomponents.js/src/MutationObserver/MutationObserver';
+import 'webcomponents.js/src/CustomElements/v1/CustomElements';
