347ec3d58a8d30303575d8ac7e5d6bf2ba47dfc6
Trey Shugart <treshugart@gmail.com>
2016-08-11 21:36:15 +1000

    test: Add test that ensures all V1 APIs exist when the library is imported.

--- test/unit.js
+++ test/unit.js
@@ -1,3 +1,15 @@
-describe('skatejs-web-components', () => {
+import '../src/index';
 
+describe('skatejs-web-components', () => {
+  it('should create a custom element with a shadow root', () => {
+    const Elem = class extends HTMLElement {
+      constructor() {
+        super();
+        this.attachShadow({ mode: 'open' });
+      }
+    };
+    window.customElements.define('x-test', Elem);
+    const elem = new Elem();
+    expect(!!elem.shadowRoot).to.equal(true);
+  });
 });
