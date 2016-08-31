137e133cc5cc81c220b361e5af87c3850b7032cf
Trey Shugart <treshugart@gmail.com>
2016-08-31 10:35:06 +1000

    feat: Update to use the versioned polyfill.
    Implements #7

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
+import 'webcomponents.js/MutationObserver';
 import 'skatejs-named-slots';
-import 'es6-collections';
-import 'webcomponents.js/src/MutationObserver/MutationObserver';
-import 'webcomponents.js/src/CustomElements/v1/CustomElements';
+import 'document-register-element';
