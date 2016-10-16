1f9a160a5dc74ba4d9e48f7960b6a58604c2c8e1
Trey Shugart <treshugart@gmail.com>
2016-10-17 00:11:46 +1100

    refactor: Add comments and eslint igores.
    Describe why loading first is necessary. Ad eslint ignores to prevent warnings about loading
    
    relative paths before absolute imports.
    
    fixes #16

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+/* eslint import/imports-first: 0 */
+
+// We load the Safari fix before document-register-element because DRE
+// overrides attachShadow() and calls back the one it finds on HTMLElement.
 import './fix/safari';
 import 'skatejs-named-slots';
 import 'document-register-element';
