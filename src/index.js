cca7f0a3f56061e86490db869afc50bf0573b504
Trey Shugart <treshugart@gmail.com>
2016-11-02 19:54:13 +1100

    feat: Use shadydom and shadycss polyfills.
    Coninue using document-register-element instead of custom-elements because it *just works*.
    
    BREAKING CHANGE: using different polyfills. Behaviour shouldn't be different, but strictly speaking,
    this is a breaking change.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // We load the Safari fix before document-register-element because DRE
 // overrides attachShadow() and calls back the one it finds on HTMLElement.
 import './fix/safari';
-import 'skatejs-named-slots';
 import 'document-register-element';
+import '@webcomponents/shadycss';
+import '@webcomponents/shadydom';
