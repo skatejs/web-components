20fb7c17b5b0a3ef448abb2ef46715dd304e2f58
Trey Shugart <treshugart@gmail.com>
2016-11-14 14:47:19 +1100

    feat: Work on getting polymer fills in.

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,28 @@
+const combine = require('webpack-combine-loaders');
 const conf = module.exports = require('skatejs-build/webpack.config');
 conf.entry['dist/index.js'] = './src/index.js';
 conf.entry['dist/index.min.js'] = './src/index.js';
+conf.module.loaders.push({
+  test: /native-shim\.js$/,
+  loader: combine([
+    {
+      loader: 'regexp-replace',
+      query: {
+        match: {
+          pattern: '/\*.*?\*/',
+          flags: 'g'
+        },
+        replaceWith: ''
+      }
+    }, {
+      loader: 'wrap',
+      query: 'eval'
+    }
+  ])
+});
+conf.wrap = {
+  eval: {
+    before: 'var body = (function(){/*',
+    after: '*/}).toString();\nbody = body.slice(body.indexOf(\'*\') + 1, body.lastIndexOf(\'*\'));\neval(body);'
+  }
+};
