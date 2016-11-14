fda4c7ede35bbf99d8047c5f3c53239ec40b8d42
Trey Shugart <treshugart@gmail.com>
2016-11-14 14:59:05 +1100

    Revert "fix: Fix issue breaking chrome native by including the native-shim."
    This reverts commit 57f9077cdad46b3f9d66135a37d2f253fedf1f33.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 // We load the Safari fix before document-register-element because DRE
 // overrides attachShadow() and calls back the one it finds on HTMLElement.
 import './fix/safari';
-import '@webcomponents/custom-elements/src/native-shim';
 import '@webcomponents/custom-elements';
 import '@webcomponents/shadydom';
 import '@webcomponents/shadycss';
