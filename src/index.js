9585f2df214b63c04de755a7105b5f6de16c60b7
Trey Shugart <treshugart@gmail.com>
2016-11-14 14:59:15 +1100

    Revert "feat: Use polymer custom element polyfill instead of document-register-element moving forward."
    This reverts commit 11b1a7b4f1b478da2b738f39ebc51552b3035060.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 // We load the Safari fix before document-register-element because DRE
 // overrides attachShadow() and calls back the one it finds on HTMLElement.
 import './fix/safari';
-import '@webcomponents/custom-elements';
+import 'document-register-element/build/document-register-element';
+
+// You must include the ShadyDOM polyfill *before* ShadyCSS. It seems that some
+// functionality in ShadyCSS requires ShadyDOM already be loaded.
 import '@webcomponents/shadydom';
 import '@webcomponents/shadycss';
