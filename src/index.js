a565e6bfe8dec9c3e40d60eb4deb0d4b7614bbdb
Trey Shugart <treshugart@gmail.com>
2016-11-14 14:59:37 +1100

    Revert "fix: Fix to import the browser version of document-register-element."
    This reverts commit 6254538340e9e0390f03fa810f5c06404e36e48f.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // We load the Safari fix before document-register-element because DRE
 // overrides attachShadow() and calls back the one it finds on HTMLElement.
 import './fix/safari';
-import 'document-register-element/build/document-register-element';
+import 'document-register-element';
 import '@webcomponents/shadydom';
 import '@webcomponents/shadycss';
