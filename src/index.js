342736334b35c16dc35b3a7ffefbd1cfe4eca66a
Trey Shugart <treshugart@gmail.com>
2016-11-14 14:58:49 +1100

    Revert "fix: Fix issues with the new custom-element polyfill to only execute the native-shim if it is runnin"
    This reverts commit a7e49860e4c1eac020b690e56f6c332110f91904.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 // We load the Safari fix before document-register-element because DRE
 // overrides attachShadow() and calls back the one it finds on HTMLElement.
 import './fix/safari';
+import '@webcomponents/custom-elements/src/native-shim';
 import '@webcomponents/custom-elements';
 import '@webcomponents/shadydom';
 import '@webcomponents/shadycss';
-
-if (~window.customElements.define.toString().indexOf('[native code]')) {
-  require('@webcomponents/custom-elements/src/native-shim');
-}
