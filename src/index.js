775302daecbd3ba742cbfcf04f3fe4b67753480a
Trey Shugart <treshugart@gmail.com>
2017-01-23 08:48:54 +1100

    fix: Fix in IE11 because the promise polyfill was mising. Remove safari fixes and force sd polyfill

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-// We load the Safari fix first because the custom element polyfill overrides
-// attachShadow() to observe the shadow root.
-require('./fix/safari');
+// Polyfill Promise in IE <= 11
+require('es6-promise').polyfill();
 
 // We have to include this first so that it can patch native.
 require('./native-shim');
@@ -8,5 +7,20 @@ require('./native-shim');
 // These must appear in this order. The ShadyCSS polyfill requires that the
 // ShadyDOM polyfill be loaded first.
 require('@webcomponents/custom-elements/src/custom-elements');
+
+// Force the polyfill in Safari 10.0.0 and 10.0.1.
+const { navigator } = window;
+const { userAgent } = navigator;
+const safari = userAgent.indexOf('Safari/60') !== -1;
+const safariVersion = safari && userAgent.match(/Version\/([^\s]+)/)[1];
+const safariVersions = [0, 1].map(v => `10.0.${v}`).concat(['10.0']);
+
+if (safari && safariVersions.indexOf(safariVersion) > -1) {
+  window.ShadyDOM = { force: true };
+}
+
+// Load Shady DOM after forcing in Safari.
 require('@webcomponents/shadydom/src/env');
+
+// Load ShadyCSS last because it requires ShadyDOM.
 require('@webcomponents/shadycss/src/ShadyCSS');
--- test/unit.js
+++ test/unit.js
@@ -1,10 +1,8 @@
 /* eslint-env jasmine, mocha */
 
 import '../src/index';
-import patch from '../src/fix/safari';
 
-const { body } = document;
-const { customElements, Element, HTMLElement } = window;
+const { customElements, HTMLElement } = window;
 
 describe('skatejs-web-components', () => {
   it('should create a custom element with a shadow root', () => {
@@ -17,59 +15,5 @@ describe('skatejs-web-components', () => {
     customElements.define('x-test-1', Elem);
     const elem = new Elem();
     expect(!!elem.shadowRoot).to.equal(true);
-  });
-
-  patch && describe('#16 - safari recalc bug', () => {
-    function getDescriptor (name) {
-      return Object.getOwnPropertyDescriptor(Element.prototype, name) ||
-        Object.getOwnPropertyDescriptor(HTMLElement.prototype, name);
-    }
-
-    it('configurable', () => {
-      expect(getDescriptor('attachShadow').configurable).to.equal(true);
-    });
-
-    it('enumerable', () => {
-      expect(getDescriptor('attachShadow').enumerable).to.equal(true);
-    });
-
-    it('writable', () => {
-      expect(getDescriptor('attachShadow').writable).to.equal(true);
-    });
-
-    it('functionality', (done) => {
-      const div = document.createElement('div');
-      div.attachShadow({ mode: 'open' });
-
-      const style = document.createElement('style');
-      div.shadowRoot.appendChild(style);
-
-      const p = document.createElement('p');
-      div.shadowRoot.appendChild(p);
-      p.appendChild(document.createTextNode('testing'));
-
-      body.appendChild(div);
-      // Wait for the first flow / layout the style element will cause.
-      setTimeout(() => {
-        // Then set the style content.
-        style.textContent = 'p { border: 1px solid black; }';
-
-        // We must wait until the next reflow / relayout updating the style element causes to then
-        // check if it appolied.
-        setTimeout(() => {
-          expect(window.getComputedStyle(p).borderWidth).to.equal('1px');
-          style.textContent = '';
-          setTimeout(() => {
-            expect(window.getComputedStyle(p).borderWidth).to.equal('0px');
-            style.textContent = 'p { border: 1px solid black; }';
-            setTimeout(() => {
-              expect(window.getComputedStyle(p).borderWidth).to.equal('1px');
-              body.removeChild(div);
-              done();
-            });
-          });
-        });
-      });
-    });
   });
 });
